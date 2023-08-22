b56cd83521929364b72fb6556b0c320b5b09e2c2
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2023-08-22 12:17:19 +0530

    Added counter functionality

--- app/src/main/java/com/example/sdpdgitdemo/MainActivity.kt
+++ app/src/main/java/com/example/sdpdgitdemo/MainActivity.kt
@@ -3,10 +3,23 @@ package com.example.sdpdgitdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cButton: Button
+    private lateinit var cTextView: TextView
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cButton = findViewById(R.id.buttonCounter)
+        cTextView=findViewById(R.id.textViewCounter)
+
+        cButton.setOnClickListener {
+            count++
+            cTextView.text = count.toString()
+        }
+
     }
 }
